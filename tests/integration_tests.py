52297b9921a128bf308dfd4e20a9d7f506ab1ebf
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-08-02 16:42:02 +0200

    - setup pytest - extract integration test

---
+++ tests/integration_tests.py
@@ -0,0 +1,29 @@
+import glob
+import traceback
+
+from icecream import ic
+
+from pagexml.model.physical_document_model import pretty_print_textregion
+from pagexml.parser import parse_pagexml_file
+
+
+def main():
+    pagexml_basedir = "../golden-agents/pagexml/"
+    testdirs = glob.glob(pagexml_basedir + '[0-9AN]*')
+    # testdirs = glob.glob(pagexml_basedir + '10025*')
+    assert len(testdirs) > 0
+    for d in testdirs:
+        files = glob.glob(d + '/*.xml')
+        assert len(files) > 0
+        for fname in files:
+            ic(fname)
+            print(fname)
+            try:
+                scan_doc = parse_pagexml_file(fname)
+                pretty_print_textregion(scan_doc, print_stats=True)
+            except Exception:
+                print(traceback.format_exc())
+
+
+if __name__ == '__main__':
+    main()
